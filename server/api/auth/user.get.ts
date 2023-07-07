8390b0fc577522298557aabe3a633d6aedb47283
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2023-07-07 20:42:47 +0300

    bug: fix imports

--- server/api/auth/user.get.ts
+++ server/api/auth/user.get.ts
@@ -1,15 +1,11 @@
 
-import { H3Event } from "h3";
-import { useStorage } from '@vueuse/core'
-// @ts-ignore
+import { H3Event, defineEventHandler } from 'h3'
 import { userTransformer } from "~/server/transformers/user"
-import { defineEventHandler } from '#imports'
 import { core } from '~/utils/core'
 
-export default defineEventHandler(async (event: H3Event) => {
+export default defineEventHandler(async (_event: H3Event) => {
 
     const user = core.getItem('user')
-
     
     return {
         user: userTransformer(user)
--- server/api/auth/users.get.ts
+++ server/api/auth/users.get.ts
@@ -3,7 +3,7 @@ import { userTransformer } from "~~/server/transformers/user"
 export default defineEventHandler((event) => {
 
   return {
-    users: event.context.auth.users.map(userTransformer)
+    users: event.context.auth?.users.map(userTransformer)
   }
 
 })
