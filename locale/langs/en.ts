c8bfd793115f67dfa6c81c2ebbfae5901e44fb6a
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2023-07-08 10:50:43 +0300

    feature: update english lang

--- locale/langs/en.ts
+++ locale/langs/en.ts
@@ -1,3 +1,136 @@
 export default {
-  welcome: 'Welcome'
+  welcome: 'Welcome',
+  button: {
+    about: "About",
+    back: "Back",
+    go: "GO",
+    home: "Home",
+    toggle_dark: "Toggle dark mode",
+    toggle_langs: "Change languages",
+    Privacy: "Our Privacy",
+    Privacy_Short: "Privacy",
+    Terms: "Our Terms & Conditions",
+    Terms_Short: "Terms",
+    Contact: "Contact us",
+    TimeLine: "Timeline",
+    Utils: "Utils",
+    submit: "Submit",
+    reset: "Reset",
+    reload: "Reload",
+    close: "Close",
+    new_content: "New content available, reload to update.",
+    offline_ready: "App ready to work offline"
+  },
+  general: {
+    Home: "Home",
+    SiteTitle: "{name}",
+    SiteDomain: "{url}",
+    navigationLinks: "Navigation Links",
+    CurrentVersion: "Current Version",
+    setLocale: "Set Default Language",
+    CheckUpdates: "Fetch Updates",
+    Services: "Services",
+    fetchingUpdates: "Get Updates",
+    languages: "Languages",
+    dark: "dark",
+    light: "light",
+    auto: "auto",
+    lightMode: "Change Light Mode",
+    open: "open",
+    close: "close",
+    holdState: "Always Open"
+  },
+  button: {
+    about: "About",
+    back: "Back",
+    go: "GO",
+    home: "Home",
+    toggle_dark: "Toggle dark mode",
+    toggle_langs: "Change languages",
+    Privacy: "Our Privacy",
+    Privacy_Short: "Privacy",
+    Terms: "Our Terms & Conditions",
+    Terms_Short: "Terms",
+    Contact: "Contact us",
+    TimeLine: "Timeline",
+    Utils: "Utils",
+    submit: "Submit",
+    reset: "Reset",
+    reload: "Reload",
+    close: "Close",
+    new_content: "New content available, reload to update.",
+    offline_ready: "App ready to work offline"
+  },
+  intro: {
+    desc: "JavaScript enthusiast, Wolf Pack brother, Ducks lover, all about reusability and improvements.",
+    hi: "Requested plugin is  {name}"
+  },
+  notfound: "Not found",
+  contact: {
+    help: "We are here",
+    offer_help: "Please let us know how we can help!",
+    accept_terms: "View our terms and conditions.",
+    terms_accepted: "I accept the terms and conditions.",
+    name: {
+      label: "Your name *",
+      hint: "Name and surname",
+      placeholder: "What is your name"
+    },
+    email: {
+      label: "Your email address *",
+      hint: "Your email",
+      placeholder: "In order to get back to you."
+    },
+    message: {
+      label: "Your message *",
+      hint: "Input your message",
+      placeholder: "Tell us how we can help"
+    },
+    projects: {
+      label: "Project Name *",
+      placeholder: "What project are you referencing?"
+    }
+  },
+  navigation: {
+    HomePageTitle: "Home Page",
+    HomePageCaption: "Home Page",
+    AuthPageTitle: "Authentications",
+    AuthPageCaption: "Create Username / Login",
+    AboutPageTitle: "About us",
+    AboutPageCaption: "About us",
+    ContactPageTitle: "Contact us",
+    ContactPageCaption: "Contact us",
+    TermsPageTitle: "Terms & Conditions",
+    TermsPageCaption: "Terms and conditions",
+    BlogPageTitle: "Our Blog",
+    BlogPageCaption: "Posts & Articles",
+    ProductsPageTitle: "Products",
+    ProductsPageCaption: "Our Products",
+    PrivacyPageTitle: "Privacy",
+    PrivacyPageCaption: "Privacy Page",
+    ServicesPageTitle: "Services",
+    ServicesPageCaption: "Our Services",
+    SettingsPageTitle: "Settings Page",
+    SettingsPageCaption: "Display Settings",
+    TranslationPageTitle: "Translations",
+    TranslationPageCaption: "Manage your Translations",
+    GlossaryPageTitle: "Glossary",
+    GlossaryPageCaption: "Our Glossary Page",
+    MediaPageTitle: "Media",
+    MediaPageCaption: "Our Media Page"
+  },
+  about: {
+    title: "Waelio",
+    par10: "A diverse/eclectic leader the in the digital world, with over couple of decades of professional success.",
+    par20: "As leader and a mentor for company of professionals, the bar sets to the highest level in the market.",
+    par21: "The logic behind our success extends beyond deep understanding of the products requirements,",
+    par22: "but rather the interpersonal skills with the team, the collective problem-solving skills and the capacity to acquire knowledge.",
+    par31: "For a over a decade, I assumed numerous executive business positions, nevertheless, I've always had as affinity for IoT.",
+    par32: "which is perfect for Iam an avid tracker of technological advancements, research and development.",
+    par33: "I have builtin love for r&d, which enabled me to successfully provide solutions and products such as Crm(s), Erp(s),",
+    par40: "Inventory Managers and Accounting Managers –some of which are being used today - for numerous prominent clients such Nike, PGA,",
+    par51: "NBA and other well-established businesses in Israel as well.",
+    par52: "I am a multilingual English, Arabic and Hebrew speaker, which are my native languages, however, I have learned to speak basic Russian as well.",
+    par53: "When possible, I am semi-professional aerial/sports photographer and video producer."
+  }
 }
