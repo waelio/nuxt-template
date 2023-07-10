e15eb9765e9026f9b49397d27c060071fb8c2b8b
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2023-07-10 14:20:53 +0300

    fix: add promises to refreshTokents

--- server/db/refreshTokens.ts
+++ server/db/refreshTokens.ts
@@ -2,15 +2,15 @@
 import prisma from './index'
 import { TReshreshToken } from "../../types";
 
-export const createRefreshToken = (refreshToken: { token: any; userId: any; }) => {
-  return prisma.refreshToken?.create({
+export const createRefreshToken = async (refreshToken: { token: any; userId: any; }) => {
+  return await prisma.refreshToken?.create({
     data: refreshToken
   })
 }
 
-export const getRefreshTokenByToken = (rtoken: string) => {
+export const getRefreshTokenByToken = async (rtoken: string) => {
   if (!rtoken) return 'null'
-  return prisma.refreshToken?.findUnique({
+  return await prisma.refreshToken?.findUnique({
     where: {
       token: rtoken
     }
@@ -18,8 +18,8 @@ export const getRefreshTokenByToken = (rtoken: string) => {
 }
 
 
-export const removeRefreshToken = (rtoken: TReshreshToken) => {
-  return prisma.refreshToken?.delete({
+export const removeRefreshToken = async (rtoken: TReshreshToken) => {
+  return await prisma.refreshToken?.delete({
     where: {
       id: rtoken.id
     }
