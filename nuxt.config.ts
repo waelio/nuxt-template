27e94c6c668b4bef69883fea1269dc1b5c9dd558
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2023-07-13 08:01:33 +0300

    bug: pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -65,17 +65,17 @@ export default defineNuxtConfig({
       theme_color: '#ffffff',
       icons: [
         {
-          src: 'pwa-192x192.png',
+          src: 'quasar-logo.png',
           sizes: '192x192',
           type: 'image/png',
         },
         {
-          src: 'pwa-512x512.png',
+          src: 'quasar-logo.png',
           sizes: '512x512',
           type: 'image/png',
         },
         {
-          src: 'pwa-512x512.png',
+          src: 'quasar-logo.png',
           sizes: '512x512',
           type: 'image/png',
           purpose: 'any maskable',
