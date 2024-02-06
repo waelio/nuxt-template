292d53cac5305cef2581f31f31336e9c8ae383c5
Peace Marshal <88880103+peace2074@users.noreply.github.com>
2024-02-06 15:59:37 +0200

    Update index.ts
    type langs

--- locale/index.ts
+++ locale/index.ts
@@ -1,6 +1,10 @@
-import en from './langs/en.json'
-import ar from './langs/ar.json'
-import he from './langs/he.json'
+import en_raw from './langs/en.json'
+import ar_raw from './langs/ar.json'
+import he_raw from './langs/he.json'
+
+const en:typeof en_raw = en_raw
+const ar:typeof ar_raw = ar_raw
+const he:typeof he_raw = he_raw
 
 export { en }
 export { ar }
