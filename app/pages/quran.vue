fa5d7b3a36a7cb1715bec8377a930f94f8d03732
Peace Marshal <88880103+peace2074@users.noreply.github.com>
2024-12-21 21:45:56 +0200

    test

--- app/pages/quran.vue
+++ app/pages/quran.vue
@@ -6,13 +6,14 @@ type ONET = {
 }
 
 const book: ONET[] = await useQuran()
+const bookSize = book.length
 </script>
 <template>
     <QPage padding class="fit rtl">
         <div class="flex column space-around items-center">
-            <q-list padding bordered class="fit rounded-borders" v-for="(one, number) in book" :key="one.chapter">
-                <q-expansion-item dense dense-toggle expand-separator :label="number">
-                    <q-card v-for="(V, index) in one" :key="index + 1">
+            <q-list padding bordered class="fit rounded-borders" v-for="(one, X) in book" :key="one.chapter">
+                <q-expansion-item dense dense-toggle expand-separator :label="X">
+                    <q-card v-for="(V, X) in one" :key="x + 1">
                         <q-card-section class="text-center justify fit">
                             <p>{{ V.text }}</p>
                         </q-card-section>
