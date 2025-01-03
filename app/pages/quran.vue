5f88f32e7793427a40ea0872df00c0a8e949cbda
Peace Marshal <88880103+peace2074@users.noreply.github.com>
2025-01-03 08:14:16 +0200

    Update quran.vue

--- app/pages/quran.vue
+++ app/pages/quran.vue
@@ -18,15 +18,15 @@ const bookSize = bBook.length
 </script>
 <template>
     <QPage padding class="fit rtl">
-  <!-- <pre>{{ bBook[0] }}</pre> -->
+  <pre>{{ bBook[0] }}</pre>
         <div class="flex column space-around fit items-center">
             <q-list padding bordered class="fit rounded-borders" v-for="one in bBook" :label="one.Name" :key="one.Index">
                 <q-expansion-item dense dense-toggle expand-separator :label="one.Name">
                     <q-card v-for="V in one" :key="V.Index">
 												<p>{{ V.Name }}</p>
-                        <q-card-section class="text-center justify fit" v-for="wrd in V.versus">
+                        <!-- <q-card-section class="text-center justify fit" v-for="wrd in V.versus">
                             <p>{{ wrd }}</p>
-                        </q-card-section>
+                        </q-card-section> -->
                     </q-card>
                 </q-expansion-item>
             </q-list>
