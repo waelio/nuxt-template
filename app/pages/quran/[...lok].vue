7101c3c515a5186ee3f7d648760e45c67c116cae
Peace Marshal <88880103+peace2074@users.noreply.github.com>
2025-02-05 22:03:45 +0200

    fix bug

--- app/pages/quran/[...lok].vue
+++ app/pages/quran/[...lok].vue
@@ -1,9 +1,7 @@
 <script lang="ts" async setup>
 import { useHead, useNuxtApp, useRoute } from 'nuxt/app';
-import { is } from 'quasar';
-import { computed, Ref, ref, watchEffect } from 'vue';
+import { computed, ref, watchEffect } from 'vue';
 import { useRouter } from 'vue-router';
-import { isNumber } from 'waelio-utils';
 import { AlFateha, appDescription, appName } from '../../constants/global';
 
 export type ONET = {
@@ -20,47 +18,44 @@ useHead({
     ogTitle: appName,
     ogDescription: appDescription,
 });
-
 const route = useRoute()
 const lok = ref(route.params?.lok || 1)
 
-const sglok = (off = 1): number => {
-    return (isNumber(off)) ? lok.value = +off : 1
 
-}
 watchEffect(() => {
-    sglok(+route.params.lok)
+    lok.value = +route.params.lok || 1
 })
 
 
 
 const nuxtApp = useNuxtApp()
 const Quran: ONET[] = nuxtApp.payload.data['B6H5jvHlMH'].data
-const sura = computed(() => Quran[lok.value - 1] as unknown as number)
-const Verses: Ref<string[]> = computed(() => sura.value.Verses)
+const sura = computed(() => Quran[(lok.value - 1)] as unknown as number)
+const Verses = computed(() => sura.value.Verses)
 const cleanText = computed(() => JSON.stringify(Verses.value).replaceAll(',', ' ♦ '))
-
 </script>
 <template>
     <QPage padding class=" rtl">
         <div class="q-mt-md q-gutter-md">
             <q-card class="text-sm">
-                <div class="q-pa-md flex h-50">
+                <q-card-section class="flex h-50">
+                    <div>
 
-                    <h4 class="text-h3">{{ sura.Name }}</h4>
+                        <h4 class="text-h3">{{ sura.Name }}</h4>
+                    </div>
                     <div>
                         <h4 class="capitalize">{{ sura.TotalVerses }}</h4>
                         <h4 class="capitalize text-h6">{{ sura.Location }}</h4>
                     </div>
 
-                </div>
+                </q-card-section>
             </q-card>
             <q-card class="q-mt-xs">
                 <q-card-section>
-                    <p class="capitalize block just fit verse">
-                        <span class="verse text-center">
-                            <span v-if="lok.value !== 1">{{ AlFateha }}</span></span> ♦{{ cleanText }}
-                    </p>
+                    <h3>{{ AlFateha }}</h3>
+                </q-card-section>
+                <q-card-section>
+                    <p class="capitalize block just fit verse"> {{ cleanText }}</p>
                 </q-card-section>
             </q-card>
         </div>
---
+++ app/pages/quran/index.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+    <div>
+        <q-menu>
+            <q-list style="min-width: 100px">
+                <q-item clickable v-close-popup>
+                    <q-item-section>New tab</q-item-section>
+                </q-item>
+                <q-separator />
+                <q-item clickable v-close-popup>
+                    <q-item-section>New incognito tab</q-item-section>
+                </q-item>
+            </q-list>
+        </q-menu>
+        <slot />
+    </div>
+</template>
